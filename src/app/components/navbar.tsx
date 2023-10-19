e32161a3ee54c0f19f5faca29f583cf8b4458e4c
Muhammad Abdullah <124899260+Pkabdullah@users.noreply.github.com>
2023-10-19 10:31:30 -0700

    Update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ import { FiDownload } from 'react-icons/fi';
 
 const Navbar = () => {
     return (
-        <div className="bg-white z-50 sticky top-0">
+        <div className="bg-white ">
             <header className="text-gray-600 body-font ">
                 <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center ">
                     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
